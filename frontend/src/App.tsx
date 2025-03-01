5bf3060497d42d1b77049886c9eff32e7e8d148a
Harrison Cole <harrydcole2@gmail.com>
2025-02-28 22:39:58 -0700

    Pantry as a series of rows with more accurate data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Pantry from "./pages/Pantry";
 import Layout from "./pages/Layout";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      {/* <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} /> */}
       <Route path="/" element={<Layout />}>
         <Route path="/pantry" element={<Pantry />} />
         <Route
           path="/recipes"
-          element={<div>Recipes Page (Coming Soon)</div>}
+          element={
+            <div>
+              Recipes Page (Coming Soon)<Button>hello</Button>
+            </div>
+          }
         />
         <Route path="/help" element={<div>Help Page (Coming Soon)</div>} />
       </Route>
