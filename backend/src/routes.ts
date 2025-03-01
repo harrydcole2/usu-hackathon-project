dcf940aafd93505ea07e2f89e3428110723652ce
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-03-01 10:13:27 -0700

    added endpoints and hookups to manage receipt objects

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -120,41 +120,66 @@ export default function setupRoutes(app: Express, dependencies: Dependencies) {
 
   // Receipt Routes
 
+  // Gets all receipts for users
   app.get("/receipts", async (req: JWTRequest, res) => {
     try {
+      const targetUser = req.auth?.userId;
 
+      const receipts = await dependencies.receipts.getReceiptByUser(targetUser);
+      res.send({ receipts })
     } catch (error) {
       console.error(error);
       res.status(500).send("Server Error");
     }
-  })
+  });
 
-  app.post('/receipts', async (req: JWTRequest, res) => {
+  // I'm expecting an object that looks like { receiptData: { name, date } }
+  app.post("/receipts", async (req: JWTRequest, res) => {
     try {
+      const receiptData = req.body?.receiptData;
+      const targetUser = req.auth?.userId;
 
+      const newReceipt = await dependencies.receipts.createReceipt(
+        receiptData.name,
+        receiptData.date,
+        targetUser
+      );
+      res.send({
+        message: "Successfully Created New Receipt",
+        newReceiptId: newReceipt.id,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).send("Server Error");
     }
-  })
+  });
 
-  app.put('/receipts', async (req: JWTRequest, res) => {
+  // I'm expecting an object that looks like { receiptData: { id, name, date }}
+  app.put("/receipts", async (req: JWTRequest, res) => {
     try {
+      const receiptData = req.body?.receiptData
+      const targetUser = req.auth?.userId;
 
+      await dependencies.receipts.updateReceipt(receiptData.id, receiptData.date, receiptData.name, targetUser)
+      res.send('Update on Receipt Complete')
     } catch (error) {
       console.error(error);
       res.status(500).send("Server Error");
     }
-  })
+  });
 
-  app.delete('/receipts', async (req: JWTRequest, res) => {
+  app.delete("/receipts/:receiptId", async (req: JWTRequest, res) => {
     try {
+      const targetUser = req.auth?.userId;
+      const receiptId = req.params?.receiptId
 
+      await dependencies.receipts.deleteReceipt(Number(receiptId), targetUser)
+      res.send('Successfully Deleted Recipe')
     } catch (error) {
       console.error(error);
       res.status(500).send("Server Error");
     }
-  })
+  });
 
   // Authentication Routes
 
