00dd44d3486fac23e7e7db2fca4790a555a04e17
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-02-28 21:18:05 -0700

    fix bugs lol

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 import setupRoutes from "./routes";
 import Config from "./config";
 import { expressjwt } from "express-jwt";
+import bodyParser = require("body-parser");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+
 
 const config = new Config();
 
@@ -17,8 +20,11 @@ app.use(
   expressjwt({
     secret: process.env.JWT_KEY ?? 'secretDONOTUSETHIS',
     algorithms: ["HS256"],
-  }).unless({ path: ["/login"] })
+  }).unless({ path: ["/login", "/newUser"] })
 );
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Install Routes to the Express system
 setupRoutes(app, dependencies);
